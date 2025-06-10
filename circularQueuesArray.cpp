6440e3652ffb42aaf63a9cc2dc202fbc8d451e47
mansoor <t.alyamani95@gmail.com>
2025-06-10 11:43:44 +0700

    Check for overflow condition

--- circularQueuesArray.cpp
+++ circularQueuesArray.cpp
@@ -21,4 +21,11 @@ public:
         cout << "Enter a number: ";
         cin >> num;
 
-        
+        // Check for overflow condition
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+
+       
