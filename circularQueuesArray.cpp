bad779124ace33e501a2ce2924f699b8ed755987
mansoor <t.alyamani95@gmail.com>
2025-06-10 11:43:14 +0700

    Function to insert an element into the queue

--- circularQueuesArray.cpp
+++ circularQueuesArray.cpp
@@ -4,6 +4,21 @@ using namespace std;
 class Queue
 {
 private:
-    static const int max = 5;         
-    int FRONT, REAR;                 
-   
+    static const int max = 5;         // Maximum size of the queue
+    int FRONT, REAR;                  // Pointers to front and rear
+    int queue_array[max];             // Array to store queue elements
+
+public:
+    Queue()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+    void insert()
+    {
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+
+        
